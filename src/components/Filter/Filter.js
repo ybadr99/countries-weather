334e881237a9c736dd2798aa885beacbe20a4977
ybadr99 <yousefbadr909@gmail.com>
2023-07-02 06:46:32 +0300

    add filter by name

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -8,12 +8,17 @@ import './Filter.scss';
 const Filter = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedRegion, setSelectedRegion] = useState('Filter by region');
+  const [searchInput, setSearchInput] = useState('');
 
   const allRegions = ['All Regions', ...new Set(data.map((el) => el?.region))];
   const handleRegion = (e, region) => {
     e.stopPropagation();
     setSelectedRegion(region);
     setIsOpen(false);
+  };
+
+  const handleCountryChange = (e) => {
+    setSearchInput(e.target.value);
   };
 
   const dispatch = useDispatch();
@@ -29,28 +34,53 @@ const Filter = () => {
         dispatch(setCountries(selectedRegionArray));
       }
     }
-  }, [selectedRegion, setSelectedRegion, dispatch]);
+    if (searchInput === '') {
+      dispatch(setCountries(data));
+    } else if (searchInput) {
+      // eslint-disable-next-line max-len
+      const filteredCountry = data.filter((el) => el.name.toLowerCase().includes(searchInput.toLowerCase()));
+      dispatch(setCountries(filteredCountry));
+    }
+  }, [
+    selectedRegion,
+    setSelectedRegion,
+    searchInput,
+    setSearchInput,
+    dispatch,
+  ]);
 
   return (
-    <button type="button" className="filter" onClick={() => setIsOpen(!isOpen)}>
-      <div>
-        {selectedRegion}
-        {isOpen ? <FaArrowUp /> : <FaArrowDown />}
-      </div>
-      {isOpen && (
-        <div className="regions">
-          {allRegions.map((region, idx) => (
-            <button
-              type="button"
-              onClick={(e) => handleRegion(e, region)}
-              key={idx}
-            >
-              {region}
-            </button>
-          ))}
+    <div className="search">
+      <input
+        type="text"
+        placeholder="Search country"
+        onChange={handleCountryChange}
+        value={searchInput}
+      />
+      <button
+        type="button"
+        className="filter"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <div>
+          {selectedRegion}
+          {isOpen ? <FaArrowUp /> : <FaArrowDown />}
         </div>
-      )}
-    </button>
+        {isOpen && (
+          <div className="regions">
+            {allRegions.map((region, idx) => (
+              <button
+                type="button"
+                onClick={(e) => handleRegion(e, region)}
+                key={idx}
+              >
+                {region}
+              </button>
+            ))}
+          </div>
+        )}
+      </button>
+    </div>
   );
 };
 
